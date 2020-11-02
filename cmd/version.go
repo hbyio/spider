471f30954d536acfb06c324fb010bd9c97082bc6
Matthieu <matt@pilot.pm>
2020-11-02 16:40:54 +0100

    Tidy deps + cmd version

---
+++ cmd/version.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	version = "v0.3"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Spiderhouse",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Working with OutOrStdout/OutOrStderr allows us to unit test our command easier
+		out := cmd.OutOrStdout()
+
+		// Print the final resolved value from binding cobra flags and viper config
+		fmt.Fprintf(out, "Spiderhouse %s\n", version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
